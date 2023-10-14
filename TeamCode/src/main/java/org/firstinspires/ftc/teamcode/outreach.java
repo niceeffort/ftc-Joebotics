4a56655471cafa01cb7bca27eb84bf9326c05c67
AlexandraNF <115591497+AlexandraNF@users.noreply.github.com>
2023-10-14 10:05:16 -0500

    added launcher and claw code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outreach.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outreach.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Outreach")
 public class outreach extends LinearOpMode {
@@ -13,14 +14,30 @@ public class outreach extends LinearOpMode {
         DcMotor launcher = hardwareMap.dcMotor.get("launcher");
         DcMotor bk_rt = hardwareMap.dcMotor.get("bk_rt");
         DcMotor bk_lt = hardwareMap.dcMotor.get("bk_lt");
+        Servo claw = hardwareMap.servo.get("claw");
 
         bk_rt.setDirection(DcMotorSimple.Direction.REVERSE);
+        launcher.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
         while(opModeIsActive()){
             bk_lt.setPower(-gamepad1.left_stick_y);
             bk_rt.setPower(-gamepad1.right_stick_y);
+
+            if(gamepad1.a){
+                launcher.setPower(1.0);
+            }
+            else{
+                launcher.setPower(0);
+            }
+
+            if(gamepad1.left_bumper){
+                claw.setPosition(0);
+            }
+            else if (gamepad1.right_bumper) {
+                claw.setPosition(1);
+            }
 
             telemetry.addData("left stick y", -gamepad1.left_stick_y);
             telemetry.addData("right stick y", -gamepad1.right_stick_y);
