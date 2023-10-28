ce197b841401dc1f8d4b67af226c080fda59464f
AlexandraNF <115591497+AlexandraNF@users.noreply.github.com>
2023-10-28 10:34:41 -0500

    made arm move

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
+//import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
@@ -20,6 +21,9 @@ public class Drive extends LinearOpMode {
         DcMotor ft_lt = hardwareMap.dcMotor.get("front_left_motor");
         DcMotor ft_rt = hardwareMap.dcMotor.get("front_right_motor");
         DcMotor bk_rt = hardwareMap.dcMotor.get("back_right_motor");
+        DcMotor arm_rt = hardwareMap.dcMotor.get("arm_rt");
+        DcMotor arm_lt = hardwareMap.dcMotor.get("arm_lt");
+        // servo = hardwareMap.get(Servo.class, "claw");
 
         // These may be robot dependant
         bk_lt.setDirection(DcMotor.Direction.REVERSE);
@@ -30,8 +34,8 @@ public class Drive extends LinearOpMode {
 
         // Set this to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
 
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
@@ -41,11 +45,15 @@ public class Drive extends LinearOpMode {
         ft_lt.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         ft_rt.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         bk_rt.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm_rt.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm_lt.setMode(DcMotor.RinMode.STOP_AND_RESET_ENCODER);
 
         bk_lt.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         ft_lt.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         ft_rt.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         bk_rt.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm_rt.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm_lt.setMode(DcMotor.RinMode.RUN_USING_ENCODER);
         */
 
         telemetry.addData("Press Start When Ready","");
@@ -56,6 +64,19 @@ public class Drive extends LinearOpMode {
             double left_stick_x = gamepad1.left_stick_x;
             double left_stick_y = -gamepad1.left_stick_y;
             double triggers = gamepad1.left_trigger - gamepad1.right_trigger;
+            boolean dpadDown = gamepad1.dpad_down;
+            boolean dpadUp = gamepad1.dpad_up;
+
+            if (dpadUp == true){
+                arm_rt.setPower(0.75);
+                arm_lt.setPower(0.75);
+            } else if (dpadDown == true){
+                arm_rt.setPower(-0.60);
+                arm_lt.setPower(-0.60);
+            } else if (dpadDown == false && dpadUp == false) {
+                arm_rt.setPower(0);
+                arm_lt.setPower(0);
+            }
 
             if (Math.abs(triggers) < 0.05) triggers = 0;
             double bk_lt_power = -left_stick_x + left_stick_y - triggers;
@@ -67,7 +88,7 @@ public class Drive extends LinearOpMode {
             //double botHeading = imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS).firstAngle;
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-            // Rotate the movement direction counter to the bot's rotation
+            // Rotate the movement direction counter to the robot's rotation
             if(fieldCentric) {
 
                 // This can be used if the robot become uncalibrated. Point the robot away from you and press start.
@@ -102,6 +123,8 @@ public class Drive extends LinearOpMode {
             telemetry.addData("Left Stick X", left_stick_x);
             telemetry.addData("Left Stick Y", left_stick_y);
             telemetry.addData("Triggers", triggers);
+            telemetry.addData("dpadUp", dpadUp);
+            telemetry.addData("dpadDown", dpadDown);
             telemetry.update();
         }
         bk_lt.setPower(0);
