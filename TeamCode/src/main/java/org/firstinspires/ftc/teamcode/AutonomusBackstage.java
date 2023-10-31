fa8f45191f6a2a23f9a794493f2f9e9e13620076
epaletta27 <epaletta27@stjosephacademy.org>
2023-10-31 16:11:22 -0500

    Made robot go forward for 1 second

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomusBackstage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomusBackstage.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 @Autonomous(name = "AutonomusBackstage")
 public class AutonomusBackstage extends LinearOpMode {
 
@@ -14,35 +13,27 @@ public class AutonomusBackstage extends LinearOpMode {
         DcMotor bk_lt = hardwareMap.dcMotor.get("bk_lt");
         DcMotor ft_rt = hardwareMap.dcMotor.get("ft_rt");
         DcMotor ft_lt = hardwareMap.dcMotor.get("ft_lt");
-        Servo claw = hardwareMap.servo.get("claw");
         bk_lt.setDirection(DcMotor.Direction.REVERSE);
         ft_lt.setDirection(DcMotor.Direction.REVERSE);
 
         double power = .5;
-        double backward_power = -.5;
         boolean done = false;
         waitForStart();
 
         while(opModeIsActive() && !done){
-
-           //sleep(15000);
            bk_rt.setPower(power);
            bk_lt.setPower(power);
            ft_rt.setPower(power);
            ft_lt.setPower(power);
 
-           sleep(1600);
-
+           sleep(1000);
            done = true;
-
         }
 
         bk_rt.setPower(0);
         bk_lt.setPower(0);
         ft_rt.setPower(0);
         ft_lt.setPower(0);
-
-        claw.setPosition(1);
 
     }
 }
