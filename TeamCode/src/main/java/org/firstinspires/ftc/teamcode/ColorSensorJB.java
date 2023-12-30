b626c6bb2053ae6566daafa87f6851aa271bab5c
epaletta27 <epaletta27@stjosephacademy.org>
2023-12-30 10:14:44 -0600

    Making color sensor class and test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorJB.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class ColorSensorJB {
+
+    HardwareMap hardwareMap;
+    Telemetry telemetry;
+    ColorSensor colorSensor;
+    int color;
+
+    public ColorSensorJB(LinearOpMode opMode){
+        hardwareMap = opMode.hardwareMap;
+        telemetry = opMode.telemetry;
+        colorSensor = hardwareMap.colorSensor.get("color");
+    }
+
+    public int Update(){
+
+        int red;
+        int green;
+        int blue;
+        double colorTotal;
+
+        double redPercent;
+        double greenPercent;
+        double bluePercent;
+        double colorThreshold = .4;
+
+        red = colorSensor.red();
+        green = colorSensor.green();
+        blue = colorSensor.blue();
+        colorTotal = red + green + blue;
+        redPercent = red / colorTotal;
+        greenPercent = green / colorTotal;
+        bluePercent = blue / colorTotal;
+
+        telemetry.addData("red", redPercent);
+        telemetry.addData("green", greenPercent);
+        telemetry.addData("blue", bluePercent);
+
+        if(redPercent >= colorThreshold){
+            color = 1;
+            telemetry.addLine("seeing red");
+        }
+        else if(greenPercent >= colorThreshold){
+            color = 2;
+            telemetry.addLine("seeing green");
+        }
+        else if(bluePercent >= colorThreshold){
+            color = 3;
+            telemetry.addLine("seeing blue");
+        }
+        else{
+            telemetry.addLine("seeing nothing");
+        }
+
+        telemetry.update();
+
+        return color;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveToColor.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "DriveToColor")
+public class DriveToColor extends LinearOpMode {
+    MecanumDrive myDrive = null;
+    ColorSensorJB colorSensor;
+    boolean color_found = false;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        myDrive = new MecanumDrive(this);
+        colorSensor = new ColorSensorJB(this);
+
+        waitForStart();
+        while(opModeIsActive()){
+            int color = colorSensor.Update();
+
+            if(color > 0){
+                color_found = true;
+            }
+            if(color_found){
+                myDrive.Update(0,0,0,0);
+            }
+            else{
+                myDrive.Update(0, .25, 0, 0);
+            }
+        }
+    }
+}
