416911f881683730921ce6997838e74385cc5d3a
David Schoemehl <davidschoemehl@gmail.com>
2024-01-30 16:27:53 -0600

    Color sensor classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Color.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Color {
+        int red;
+        int green;
+        int blue;
+        Color(int red_value, int green_value, int blue_value) {
+            red = red_value;
+            green = green_value;
+            blue = blue_value;
+        }
+}
