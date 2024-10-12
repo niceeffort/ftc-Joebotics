ae3f28d9fe8d959f606f5b176da017b66dc153a2
AlexandraNF <115591497+AlexandraNF@users.noreply.github.com>
2024-10-12 10:01:43 -0500

    made so robot can turn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outreach.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outreach.java
@@ -40,8 +40,10 @@ public class outreach extends LinearOpMode {
             //bk_rt.setPower(-gamepad1.right_stick_y);
 
             double leftstick_y = -gamepad1.left_stick_y;
+            double rightstick_y = -gamepad1.right_stick_y;
             bk_lt.setPower(leftstick_y * .5);
-            bk_rt.setPower(leftstick_y * .5);
+            bk_rt.setPower(rightstick_y * .5);
+
 
             if(gamepad1.a){
                 launcher.setPower(1.0);
