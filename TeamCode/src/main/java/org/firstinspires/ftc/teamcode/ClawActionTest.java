591f007f22a2180eb4341a4a4aecab56bb4b1af4
epaletta27 <epaletta27@stjosephacademy.org>
2024-10-19 10:28:48 -0500

    Action test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawActionTest.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+class Claw {
+    private Servo clawServo;
+
+    public Claw(HardwareMap hardwareMap){
+        clawServo = hardwareMap.get(Servo.class, "claw");
+    }
+
+    public Action open(){
+        return new Action() {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet){
+                if(!initialized){
+                    clawServo.setPosition(0.5);
+                    initialized = true;
+                }
+                double servoPosition = clawServo.getPosition();
+                return servoPosition >= .5;
+            }
+        };
+    }
+}
+
+@Autonomous(name = "ClawActionTest")
+public class ClawActionTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException{
+        Claw clawAction = new Claw(hardwareMap);
+
+        waitForStart();
+        Actions.runBlocking(clawAction.open());
+    }
+}
