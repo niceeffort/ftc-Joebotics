bf415c17c3881a1afde6153d419c25d2c62e8361
epaletta27 <epaletta27@stjosephacademy.org>
2024-11-05 16:14:03 -0600

    Claw code and test for RoadRunner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+class Claw {
+    private final Servo clawServo;
+
+    public enum ClawPosition {OPEN, CLOSE}
+    private final double[] clawPositions = new double[]{1.0, 0.0};
+
+    public Claw(HardwareMap hardwareMap){
+        clawServo = hardwareMap.get(Servo.class, "claw");
+    }
+
+    public Action setPosition(ClawPosition position){
+        return new Action() {
+            private boolean initialized = false;
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    clawServo.setPosition(clawPositions[position.ordinal()]);
+                    initialized = true;
+                }
+                double servoPosition = clawServo.getPosition();
+                return servoPosition != clawPositions[position.ordinal()];
+            }
+        };
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRClawTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "RRClawTest")
+public class RRClawTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Claw myClaw = new Claw(hardwareMap);
+
+        waitForStart();
+
+        telemetry.addLine("Starting open");
+        telemetry.update();
+        Actions.runBlocking(myClaw.setPosition(Claw.ClawPosition.OPEN));
+        telemetry.addLine("Ending open");
+        telemetry.update();
+        sleep(2000);
+        telemetry.addLine("Starting close");
+        telemetry.update();
+        Actions.runBlocking(myClaw.setPosition(Claw.ClawPosition.CLOSE));
+        sleep(2000);
+        telemetry.addLine("Ending close");
+        telemetry.update();
+    }
+}
