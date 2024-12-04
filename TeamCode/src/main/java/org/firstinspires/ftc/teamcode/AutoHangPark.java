c7c579703159fe97777c496d67d90b59fa2a66bc
niceeffort <davidschoemehl@gmail.com>
2024-12-03 19:49:53 -0600

    Sample program to simplify the code for autonomous mode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoHangPark.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "AutoHangPark")
+public class AutoHangPark extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        Pose2d beginPose = new Pose2d(0, 60, Math.toRadians(-90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+        Riser myRiser = new Riser(hardwareMap);
+        Claw myClaw = new Claw(hardwareMap);
+
+        waitForStart();
+
+        TrajectoryActionBuilder drive_forward = drive.actionBuilder(beginPose).lineToY(35);
+        TrajectoryActionBuilder drive_to_bar = drive_forward.endTrajectory().fresh().lineToY(30);
+        TrajectoryActionBuilder drive_to_park = drive_to_bar.endTrajectory().fresh().strafeToConstantHeading(new Vector2d(-55, 60));
+
+        Actions.runBlocking(
+                new SequentialAction( myClaw.setPosition(Claw.ClawPosition.CLOSE),
+                                      drive_forward.build(),
+                                      myRiser.setPosition(Riser.RiserPosition.HIGH_BAR),
+                                      drive_to_bar.build(),
+                                      myRiser.setPosition(Riser.RiserPosition.DOWN),
+                                      drive_to_park.build()));
+    }
+}
