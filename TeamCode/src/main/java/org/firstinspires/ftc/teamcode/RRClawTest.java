926b745b5447e344cd0770368ed47f93fbe8815b
epaletta27 <epaletta27@stjosephacademy.org>
2024-12-07 10:48:01 -0600

    Updating riser enumerations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRClawTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRClawTest.java
@@ -35,13 +35,13 @@ public class RRClawTest extends LinearOpMode {
         //Riser
         telemetry.addLine("Starting up");
         telemetry.update();
-        Actions.runBlocking(myRiser.setPosition(Riser.RiserPosition.UP));
+        Actions.runBlocking(myRiser.setPosition(Riser.RiserPosition.TOP));
         telemetry.addLine("Ending up");
         telemetry.update();
         sleep(2000);
         telemetry.addLine("Starting down");
         telemetry.update();
-        Actions.runBlocking(myRiser.setPosition(Riser.RiserPosition.DOWN));
+        Actions.runBlocking(myRiser.setPosition(Riser.RiserPosition.BOTTOM));
         sleep(2000);
         telemetry.addLine("Ending down");
         telemetry.update();
