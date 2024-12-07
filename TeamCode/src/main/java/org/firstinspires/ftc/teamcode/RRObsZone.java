379ad555112031175214cc40bed355a38dca6e6e
epaletta27 <epaletta27@stjosephacademy.org>
2024-12-07 10:30:55 -0600

    Simplified action list, renamed riser positions, added tolerance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRObsZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRObsZone.java
@@ -1,16 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @Autonomous(name = "RRObsZone")
 public class RRObsZone extends LinearOpMode {
@@ -23,33 +19,18 @@ public class RRObsZone extends LinearOpMode {
         Riser myRiser = new Riser(hardwareMap);
         Claw myClaw = new Claw(hardwareMap);
 
+        TrajectoryActionBuilder drive_forward = drive.actionBuilder(beginPose).lineToY(35);
+        TrajectoryActionBuilder drive_to_bar = drive_forward.endTrajectory().fresh().lineToY(30);
+        TrajectoryActionBuilder drive_to_park = drive_to_bar.endTrajectory().fresh().strafeToConstantHeading(new Vector2d(-55, 60));
+
         waitForStart();
 
-        //Close claw
-        Actions.runBlocking(myClaw.setPosition(Claw.ClawPosition.CLOSE));
-
-        //Drive to bar
         Actions.runBlocking(
-                drive.actionBuilder(beginPose)
-                        .lineToY(35)
-                        .build());
-
-        //Raise riser
-        Actions.runBlocking(myRiser.setPosition(Riser.RiserPosition.HIGH_BAR));
-
-        //Drive forward more
-        Actions.runBlocking(
-                drive.actionBuilder(new Pose2d(0,35, Math.toRadians(-90)))
-                        .lineToY(30)
-                        .build());
-
-        //Lower riser
-        Actions.runBlocking(myRiser.setPosition(Riser.RiserPosition.DOWN));
-
-        //Park
-        Actions.runBlocking(
-                drive.actionBuilder(new Pose2d(0,30, Math.toRadians(-90)))
-                        .strafeToConstantHeading(new Vector2d(-55, 60))
-                        .build());
+                new SequentialAction(myClaw.setPosition(Claw.ClawPosition.CLOSE),
+                        drive_forward.build(),
+                        myRiser.setPosition(Riser.RiserPosition.HIGH_BAR),
+                        drive_to_bar.build(),
+                        myRiser.setPosition(Riser.RiserPosition.BOTTOM),
+                        drive_to_park.build()));
     }
 }
