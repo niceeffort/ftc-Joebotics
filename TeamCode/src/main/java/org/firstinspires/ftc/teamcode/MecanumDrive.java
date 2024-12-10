85982269aaea23ca325a6d6814c0d88e5fcf4d4b
mgschoemehl <65453668+mgschoemehl@users.noreply.github.com>
2024-12-10 16:53:30 -0600

    Changed headingGain from .2 to 3.5

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -84,7 +84,7 @@ public final class MecanumDrive {
         // path controller gains
         public double axialGain = 4;
         public double lateralGain = 1;
-        public double headingGain = .2; // shared with turn
+        public double headingGain = 3.5; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
