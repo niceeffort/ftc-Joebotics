3b8a74a7ef9fd612d09f2da3a4dfdb5e0c777a7f
mgschoemehl <65453668+mgschoemehl@users.noreply.github.com>
2025-01-09 16:29:24 -0600

    Used encoder for arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -17,31 +18,44 @@ public class Arm {
 
     // Enumeration for Arm Position
     public enum ArmPosition {UP, DOWN}
-    private final double[] armPositions = new double[] {-1.0,1.0};
+    private final int[] armPositions = new int[] {0,1528};
 
     public Arm(HardwareMap hardwareMap){
+
         armMotor = hardwareMap.get(DcMotorEx.class, "arm");
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setTargetPosition(armPositions[ArmPosition.UP.ordinal()]);
+        armMotor.setPower(1.0);
     }
 
     public Action setPosition(Arm.ArmPosition position) {
         return new Action() {
             private boolean initialized = false;
-            long startTime;
+            //long startTime;
 
             // This is how long you want to run the motor
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 if (!initialized) {
                     // Capture the start time
-                    startTime = System.currentTimeMillis();
+                    //startTime = System.currentTimeMillis();
                     initialized = true;
 
                     //TODO: Set power to the motor depending on the direction you want to go
-                    armMotor.setPower(armPositions[position.ordinal()]);
+                    //armMotor.setPower(armPositions[position.ordinal()]);
+                    armMotor.setTargetPosition(armPositions[position.ordinal()]);
                 }
 
+                int currentPosition = armMotor.getCurrentPosition();
+                packet.put("arm Position", currentPosition);
+                packet.put("arm Target Position", armMotor.getTargetPosition());
+                return !(currentPosition == armPositions[position.ordinal()]);
+
+
                 // Get the current time
-                long currentTime = System.currentTimeMillis();
+                /*long currentTime = System.currentTimeMillis();
 
                 // If the time has run out you are done. If not, keep returning true and checking the time
                 if (currentTime - startTime > runTime) {
@@ -50,6 +64,7 @@ public class Arm {
                 } else {
                     return true; // Return true to continue the action
                 }
+                 */
             }
         };
     }
