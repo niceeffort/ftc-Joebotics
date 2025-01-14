71b00aeac01a39e3ee2e1de67e8f8934b9ddd0a3
AlexandraNF <115591497+AlexandraNF@users.noreply.github.com>
2025-01-14 15:28:25 -0600

    intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ftc.OverflowEncoder;
+import com.acmerobotics.roadrunner.ftc.RawEncoder;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+public class Intake {
+    public final Servo intakeServo;
+
+    public enum intakePos {IN, OUT};
+    private final double
+}
