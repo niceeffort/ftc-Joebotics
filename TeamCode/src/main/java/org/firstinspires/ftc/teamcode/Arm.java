75f16cecd8a7bf7b00cdd3fc73c757daf65e9530
epaletta27 <epaletta27@stjosephacademy.org>
2025-01-16 11:02:25 -0600

    Added hardware map constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -4,8 +4,6 @@ import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Arm {
@@ -13,58 +11,44 @@ public class Arm {
     // TODO: Add the variables you will need here
     // This is how long you want to run the motor in milliseconds.
     // The longer you run it the farther it will go
-    long runTime = 2500;
-    private final DcMotorEx armMotor;
+    long runTime = 2000;
+
+    public Arm(HardwareMap hardwareMap) {
+    }
 
     // Enumeration for Arm Position
     public enum ArmPosition {UP, DOWN}
-    private final int[] armPositions = new int[] {0,1528};
 
-    public Arm(HardwareMap hardwareMap){
+    // TODO: Make an array for the different power settings (1,-1)
 
-        armMotor = hardwareMap.get(DcMotorEx.class, "arm");
-        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        armMotor.setTargetPosition(armPositions[ArmPosition.UP.ordinal()]);
-        armMotor.setPower(1.0);
-    }
+    // TODO: Add the constructor that takes in the hardware map and store the motor in a variable
 
     public Action setPosition(Arm.ArmPosition position) {
         return new Action() {
             private boolean initialized = false;
-            //long startTime;
+            long startTime;
 
             // This is how long you want to run the motor
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 if (!initialized) {
                     // Capture the start time
-                    //startTime = System.currentTimeMillis();
+                    startTime = System.currentTimeMillis();
                     initialized = true;
 
                     //TODO: Set power to the motor depending on the direction you want to go
-                    //armMotor.setPower(armPositions[position.ordinal()]);
-                    armMotor.setTargetPosition(armPositions[position.ordinal()]);
                 }
 
-                int currentPosition = armMotor.getCurrentPosition();
-                packet.put("arm Position", currentPosition);
-                packet.put("arm Target Position", armMotor.getTargetPosition());
-                return !(currentPosition == armPositions[position.ordinal()]);
-
-
                 // Get the current time
-                /*long currentTime = System.currentTimeMillis();
+                long currentTime = System.currentTimeMillis();
 
                 // If the time has run out you are done. If not, keep returning true and checking the time
                 if (currentTime - startTime > runTime) {
-                    armMotor.setPower(0.0);
+                    //TODO: Set motor power to 0
                     return false; // Return false to stop the action
                 } else {
                     return true; // Return true to continue the action
                 }
-                 */
             }
         };
     }
