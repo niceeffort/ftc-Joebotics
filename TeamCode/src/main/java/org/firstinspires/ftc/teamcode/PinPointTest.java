f355c394e486da7963130ca9a38f480ee65a64c4
niceeffort <davidschoemehl@gmail.com>
2025-04-26 15:16:42 -0500

    Updating the position updates to the new format

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinPointTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinPointTest.java
@@ -3,6 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+import java.util.Locale;
+
 @TeleOp(name="Pinpoint Test", group="Linear Opmode")
 public class PinPointTest extends LinearOpMode {
     GoBildaPinpointDriver pinPoint;
@@ -10,16 +16,19 @@ public class PinPointTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         pinPoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
-
+        pinPoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        pinPoint.setOffsets(-84.0, -168.0, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
+        pinPoint.resetPosAndIMU();
         waitForStart();
         while (opModeIsActive()) {
             pinPoint.update();
+            Pose2D pos = pinPoint.getPosition();
             telemetry.addLine("Pressing X will reset the encoder and IMU");
             telemetry.addLine("Pressing B will reverse the encoder directions");
             telemetry.addLine("Pressing A will set the encoder directions to forward");
-            telemetry.addData("X", pinPoint.getPosX());
-            telemetry.addData("Y", pinPoint.getPosY());
-            telemetry.addData("Heading", pinPoint.getHeading());
+            telemetry.addData("X", pos.getX(DistanceUnit.INCH));
+            telemetry.addData("Y", pos.getY(DistanceUnit.INCH));
+            telemetry.addData("Old Heading", pos.getHeading(AngleUnit.DEGREES));
             telemetry.update();
 
             if (gamepad1.x) {
