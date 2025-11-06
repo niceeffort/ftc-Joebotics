8f8437145cad5deed52555108b9b580b310611c7
epaletta27 <epaletta27@stjosephacademy.org>
2025-11-06 16:08:29 -0600

    Autonomous that can drive backwards and shoot three artifacts (hopefully)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOne.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name="AutoOne")
 public class AutoOne extends LinearOpMode{
@@ -11,13 +13,42 @@ public class AutoOne extends LinearOpMode{
 
         robot.initialize(true);
 
+        DcMotor ft_pr = hardwareMap.dcMotor.get("front_pitcher");
+        DcMotor bk_pr = hardwareMap.dcMotor.get("back_pitcher");
+        CRServo cam = hardwareMap.crservo.get("cam");
+
         telemetry.addData(">", "Touch Play to run Auto");
         telemetry.update();
 
         waitForStart();
 
         if (opModeIsActive()){
-            robot.drive(-56, 0.5, 0.25);
+            robot.drive(-55, 0.5, 0.25); //Drive backward
+            cam.setPower(.5); //Turn on cam
+
+            //Shoot first ball
+            ft_pr.setPower(.5);
+            bk_pr.setPower(.5);
+            sleep(1000);
+            ft_pr.setPower(0);
+            bk_pr.setPower(0);
+            sleep(1000);
+
+            // Shoot second ball
+            ft_pr.setPower(.5);
+            bk_pr.setPower(.5);
+            sleep(1000);
+            ft_pr.setPower(0);
+            bk_pr.setPower(0);
+            sleep(1000);
+
+            //Shoot third ball
+            ft_pr.setPower(.5);
+            bk_pr.setPower(.5);
+            sleep(1000);
+            ft_pr.setPower(0);
+            bk_pr.setPower(0);
+            sleep(1000);
         }
     }
 }
