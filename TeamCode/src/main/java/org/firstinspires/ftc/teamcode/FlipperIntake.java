9717744a125c8dea83a0d3821915d38a6ad5a891
epaletta27 <epaletta27@stjosephacademy.org>
2026-01-26 10:38:50 -0600

    Cleaning up code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlipperIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlipperIntake.java
@@ -4,7 +4,6 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
@@ -25,7 +24,6 @@ public class FlipperIntake extends LinearOpMode{
         intake_one.setDirection(CRServo.Direction.REVERSE);
         intake_two.setDirection(CRServo.Direction.REVERSE);
         intake_three.setDirection(CRServo.Direction.REVERSE);
-
 
         // This part may be robot dependant
 
