1d269197fa61034db601735a42df065a6a34c425
epaletta27 <epaletta27@stjosephacademy.org>
2026-01-26 10:39:56 -0600

    Creating Pitcher Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pitcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pitcher.java
@@ -5,34 +5,37 @@ import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Pitcher {
 
-    private final DcMotorEx pitcherMotor;
-    private final DcMotorEx pitcherMotor2;
+    private final DcMotor front_pitcher;
+    private final DcMotor back_pitcher;
 
     // Enumeration for Pitcher Position
     public enum PitcherPosition {GO, STOP}
-    private final int[] pitcherPositions = new int[] {50, 0};
+    private final int[] pitcherPositions = new int[] {75, 0};
 
     public Pitcher(HardwareMap hardwareMap){
 
-        pitcherMotor = hardwareMap.get(DcMotorEx.class, "pitcher");
-        pitcherMotor2 = hardwareMap.get(DcMotorEx.class, "pitcher2");
-        pitcherMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        pitcherMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        pitcherMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        pitcherMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        pitcherMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        pitcherMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        pitcherMotor.setTargetPosition(pitcherPositions[PitcherPosition.GO.ordinal()]);
-        pitcherMotor2.setTargetPosition(pitcherPositions[PitcherPosition.GO.ordinal()]);
-        pitcherMotor.setTargetPosition(pitcherPositions[PitcherPosition.STOP.ordinal()]);
-        pitcherMotor2.setTargetPosition(pitcherPositions[PitcherPosition.STOP.ordinal()]);
-        pitcherMotor.setPower(1.0);
-        pitcherMotor2.setPower(1.0);
+        front_pitcher = hardwareMap.get(DcMotor.class, "front_pitcher");
+        back_pitcher = hardwareMap.get(DcMotor.class, "back_pitcher");
+        front_pitcher.setDirection(DcMotor.Direction.REVERSE);
+        back_pitcher.setDirection(DcMotor.Direction.REVERSE);
+        back_pitcher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        front_pitcher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        front_pitcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        back_pitcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        front_pitcher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_pitcher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        front_pitcher.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        back_pitcher.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        front_pitcher.setTargetPosition(pitcherPositions[PitcherPosition.GO.ordinal()]);
+        back_pitcher.setTargetPosition(pitcherPositions[PitcherPosition.GO.ordinal()]);
+        front_pitcher.setTargetPosition(pitcherPositions[PitcherPosition.STOP.ordinal()]);
+        back_pitcher.setTargetPosition(pitcherPositions[PitcherPosition.STOP.ordinal()]);
+        front_pitcher.setPower(1.0);
+        back_pitcher.setPower(1.0);
     }
 
     public Action setPosition(Pitcher.PitcherPosition position) {
@@ -43,14 +46,14 @@ public class Pitcher {
             public boolean run(@NonNull TelemetryPacket packet) {
                 if (!initialized) {
                     initialized = true;
-                    pitcherMotor.setTargetPosition(pitcherPositions[position.ordinal()]);
-                    pitcherMotor2.setTargetPosition(pitcherPositions[position.ordinal()]);
+                    front_pitcher.setTargetPosition(pitcherPositions[position.ordinal()]);
+                    back_pitcher.setTargetPosition(pitcherPositions[position.ordinal()]);
                 }
 
-                int currentPosition = pitcherMotor.getCurrentPosition();
+                int currentPosition = front_pitcher.getCurrentPosition();
                 packet.put("pitcher Position", currentPosition);
-                packet.put("pitcher Target Position", pitcherMotor.getTargetPosition());
-                packet.put("pitcher Target Position", pitcherMotor2.getTargetPosition());
+                packet.put("pitcher Target Position", front_pitcher.getTargetPosition());
+                packet.put("pitcher Target Position", back_pitcher.getTargetPosition());
                 return !(currentPosition == pitcherPositions[position.ordinal()]);
             }
         };
